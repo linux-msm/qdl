1f28fb02596da7b0046dee677809879ada154123
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2016-09-21 12:47:53 +0200

    qdl.h: remove unexisting function reference
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- qdl.h
+++ qdl.h
@@ -10,8 +10,6 @@ int firehose_run(int fd);
 int sahara_run(int fd, char *prog_mbn);
 void print_hex_dump(const char *prefix, const void *buf, size_t len);
 
-int content_load(const char *content_file);
-
 extern bool qdl_debug;
 
 #endif
