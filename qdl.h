b080ff24ee27559e7ba5f95f611fab1e302577fd
Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>
2024-12-17 14:01:22 -0600

    ux: Introduce progress indicator
    Not providing any user feedback during flashing is not very user
    friendly, in particular when, in some situations, it can take minutes to
    flash very large files.
    
    Introduce a progress bar for the flashing and patching steps to provide
    such feedback to the user, when stdout is determined to be a terminal of
    sufficient size (i.e. tools programmatically invoking qdl should see no
    difference).
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>

--- qdl.h
+++ qdl.h
@@ -39,10 +39,12 @@ void print_hex_dump(const char *prefix, const void *buf, size_t len);
 unsigned attr_as_unsigned(xmlNode *node, const char *attr, int *errors);
 const char *attr_as_string(xmlNode *node, const char *attr, int *errors);
 
+void ux_init(void);
 void ux_err(const char *fmt, ...);
 void ux_info(const char *fmt, ...);
 void ux_log(const char *fmt, ...);
 void ux_debug(const char *fmt, ...);
+void ux_progress(const char *fmt, unsigned int value, unsigned int size, ...);
 
 extern bool qdl_debug;
 
