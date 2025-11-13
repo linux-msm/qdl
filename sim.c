a79a572f184728dfbd6090fba73614980212512e
Igor Opaniuk <igor.opaniuk@oss.qualcomm.com>
2025-11-13 11:33:15 +0100

    Address unused parameter warnings
    Use __unused attribute to suppress compile warnings for callback
    implementations, where some parameters aren't used. For example:
    
    warning: unused parameter ‘qdl’ [-Wunused-parameter]
    ./firehose.c:257:80: warning: unused parameter ‘rawmode’ [-Wunused-parameter]
      257 | static int firehose_configure_response_parser(xmlNode *node...
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@oss.qualcomm.com>

--- sim.c
+++ sim.c
@@ -13,26 +13,32 @@ struct qdl_device_sim {
 	bool create_digests;
 };
 
-static int sim_open(struct qdl_device *qdl, const char *serial)
+static int sim_open(struct qdl_device __unused *qdl,
+		    const char __unused *serial)
 {
 	ux_info("This is a dry-run execution of QDL. No actual flashing has been performed\n");
 
 	return 0;
 }
 
-static void sim_close(struct qdl_device *qdl) {}
+static void sim_close(struct qdl_device __unused *qdl) {}
 
-static int sim_read(struct qdl_device *qdl, void *buf, size_t len, unsigned int timeout)
+static int sim_read(struct qdl_device __unused *qdl,
+		    void  __unused *buf, size_t len,
+		    unsigned int __unused timeout)
 {
 	return len;
 }
 
-static int sim_write(struct qdl_device *qdl, const void *buf, size_t len, unsigned int timeout)
+static int sim_write(struct qdl_device __unused *qdl, const void __unused *buf,
+		     size_t len, unsigned int __unused timeout)
 {
 	return len;
 }
 
-static void sim_set_out_chunk_size(struct qdl_device *qdl, long size) {}
+static void sim_set_out_chunk_size(struct qdl_device __unused *qdl,
+				   long __unused size)
+{}
 
 struct qdl_device *sim_init(void)
 {
