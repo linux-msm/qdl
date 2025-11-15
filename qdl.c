406ede8e14ad3606a151adc717a599b28e5e7275
Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>
2025-11-14 20:40:22 -0800

    qdl: Also support Sahara config XML documents
    Flattened METAs contains the relevant programmer files, as well as a
    special "sahara_config" XML file, which specifies the Sahara images and
    their image ids.
    
    Support populating the Sahara images array from this XML file as well,
    to make it easy to flash this type of builds.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>

--- qdl.c
+++ qdl.c
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <getopt.h>
+#include <libgen.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -233,6 +234,113 @@ static int decode_programmer_archive(struct sahara_image *images)
 }
 
 /**
+ * decode_sahara_config() - Attempt to decode a Sahara config XML document
+ * @images: List of Sahara images, with @images[0] populated
+ *
+ * The single blob provided in @images[0] might be a XML blob containing
+ * a sahara_config document with definitions of the various Sahara images that
+ * will be loaded. Attempt to parse this and if possible load each referenced
+ * Sahara image into the @images array.
+ *
+ * The original blob (in @images[0]) is freed once it has been consumed.
+ *
+ * Returns: 0 if no archive was found, 1 if archive was decoded, -1 on error
+ */
+static int decode_sahara_config(struct sahara_image *images)
+{
+	struct sahara_image *blob = &images[0];
+	char image_path_full[PATH_MAX];
+	const char *image_path;
+	unsigned int image_id;
+	size_t image_path_len;
+	xmlNode *images_node;
+	xmlNode *image_node;
+	char *blob_name_buf;
+	size_t base_path_len;
+	char *base_path;
+	xmlNode *root;
+	xmlDoc *doc;
+	int errors = 0;
+	int ret;
+
+	if (blob->len < 5 || memcmp(blob->ptr, "<?xml", 5))
+		return 0;
+
+	doc = xmlReadMemory(blob->ptr, blob->len, blob->name, NULL, 0);
+	if (!doc) {
+		ux_err("failed to parse sahara_config in \"%s\"\n", blob->name);
+		return -1;
+	}
+
+	blob_name_buf = strdup(blob->name);
+	base_path = dirname(blob_name_buf);
+	base_path_len = strlen(base_path);
+
+	root = xmlDocGetRootElement(doc);
+	if (xmlStrcmp(root->name, (xmlChar *)"sahara_config")) {
+		ux_err("specified sahara_config \"%s\" is not a Sahara config\n", blob->name);
+		goto err_free_doc;
+	}
+
+	for (images_node = root->children; images_node; images_node = images_node->next) {
+		if (images_node->type == XML_ELEMENT_NODE &&
+		    !xmlStrcmp(images_node->name, (xmlChar *)"images"))
+			break;
+	}
+
+	if (!images_node) {
+		ux_err("no images definitions found in sahara_config \"%s\"\n", blob->name);
+		goto err_free_doc;
+	}
+
+	for (image_node = images_node->children; image_node; image_node = image_node->next) {
+		if (image_node->type != XML_ELEMENT_NODE ||
+		    xmlStrcmp(image_node->name, (xmlChar *)"image"))
+			continue;
+
+		image_id = attr_as_unsigned(image_node, "image_id", &errors);
+		image_path = attr_as_string(image_node, "image_path", &errors);
+
+		if (image_id == 0 || image_id >= MAPPING_SZ || errors) {
+			ux_err("invalid sahara_config image in \"%s\"\n", blob->name);
+			free((void *)image_path);
+			goto err_free_doc;
+		}
+
+		image_path_len = strlen(image_path);
+		if (base_path_len + 1 + image_path_len + 1 > PATH_MAX) {
+			free((void *)image_path);
+			goto err_free_doc;
+		}
+
+		memcpy(image_path_full, base_path, base_path_len);
+		image_path_full[base_path_len] = '/';
+		memcpy(image_path_full + base_path_len + 1, image_path, image_path_len);
+		image_path_full[base_path_len + 1 + image_path_len] = '\0';
+
+		free((void *)image_path);
+
+		ret = load_sahara_image(image_path_full, &images[image_id]);
+		if (ret < 0)
+			goto err_free_doc;
+	}
+
+	xmlFreeDoc(doc);
+	free(blob_name_buf);
+
+	free(blob->ptr);
+	blob->ptr = NULL;
+	blob->len = 0;
+
+	return 1;
+
+err_free_doc:
+	xmlFreeDoc(doc);
+	free(blob_name_buf);
+	return -1;
+}
+
+/**
  * decode_programmer() - decodes the programmer specifier
  * @s: programmer specifier, from the user
  * @images: array of images to populate
@@ -270,6 +378,12 @@ static int decode_programmer(char *s, struct sahara_image *images, bool *single)
 		ret = decode_programmer_archive(images);
 		if (ret < 0)
 			return -1;
+
+		if (!ret) {
+			ret = decode_sahara_config(images);
+			if (ret < 0)
+				return -1;
+		}
 
 		*single = (ret == 0);
 
